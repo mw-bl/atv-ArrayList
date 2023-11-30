84ce4205e7540d81654b30c0773810b44c0adc0d
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2023-11-30 16:34:38 -0300

    :recycle: refactor: alterações no GerenciadorDeLivros
    mudanças nos modos de controle dos métodos e adição do metodo ExibirLivro.

--- Aula/GerenciadorDeLivros.java
+++ Aula/GerenciadorDeLivros.java
@@ -7,23 +7,36 @@ public class GerenciadorDeLivros {
         return acervo;
     }
 
-    public void addLivro(String titulo, int ano, String autor) {
+    public int addLivro(String titulo, int ano, String autor) {
         Livro l = new Livro(titulo, ano, autor);
         this.acervo.add(l);
+        return 1;
     }
 
     public void addLivro(Livro l) {
         this.acervo.add(l);
     }
 
-    public void removerLivro(String titulo) {
+    public int removerLivro(String titulo) {
         for(Livro livro: acervo) {
             if(livro.getTitulo().equals(titulo)) {
                 this.acervo.remove(livro);
-            } else {
-                System.out.println("Livro não encontrado.");
+                return 1;
             }
         }
+        return 0;
     }
-    
+
+    public int ExibirLivro(String titulo) {
+        for(Livro l : acervo) {
+            if(l.getTitulo().equals(titulo)) {
+                System.out.println("Livro: " + l.getTitulo());
+                System.out.println("Ano de Publicação: " + l.getAnoPublicacao());
+                System.out.println("Autor: " + l.getAutor());
+
+                return 1;
+            }
+        }
+        return 0;
+    }
 }
